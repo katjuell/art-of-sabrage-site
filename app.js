205f1320897fdba10a9702a17de9957948125d6d
Kat Juell <kathleenjuell@gmail.com>
2019-07-28 17:31:00 +0000

    Added additional pages sabrage.html event_tips.html and twitter cards

--- app.js
+++ app.js
@@ -1,11 +1,9 @@
-var express = require('express');
-var app = express();
-var router = express.Router();
+const express = require('express');
+const app = express();
+const router = express.Router();
+const path = __dirname;
 
-var path = __dirname;
-
-const PORT = 8080;
-const HOST = '0.0.0.0';
+const port = process.env.PORT || 8080;
 
 router.use(function (req,res,next) {
   console.log('/' + req.method);
@@ -16,14 +14,10 @@ router.get('/',function(req,res){
   res.sendFile(path + 'index.html');
 });
 
-router.get('/posts',function(req,res){
-  res.sendFile(path + '/posts/index.html');
-});
-
 app.use(express.static(path));
-app.use('/posts', express.static('/posts/articles'));
 app.use('/', router);
 
-app.listen(8080, function () {
-  console.log('Example app listening on port 8080!')
+app.listen(port, function () {
+  console.log(`Example app listening on ${port}!`)
 })
+
