4c8f87b3e7e926cfebc14375672b5370ee2c7647
Kat Juell <kathleenjuell@gmail.com>
2019-08-21 22:25:05 +0000

    Updating CDN information for move to different account. Also implementing SSL

--- ssl_renew.sh
+++ ssl_renew.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
-/usr/local/bin/docker-compose -f /home/mrcat/blog/docker-compose.yml run certbot renew \
-&& /usr/local/bin/docker-compose -f /home/mrcat/blog/docker-compose.yml kill -s SIGHUP webserver
+COMPOSE="/usr/local/bin/docker-compose --no-ansi"
+
+cd /home/mrcat/art-of-sabrage-site/
+$COMPOSE run certbot renew --dry-run && $COMPOSE kill -s SIGHUP webserver
