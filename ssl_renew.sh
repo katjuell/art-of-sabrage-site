10758aab8579aa73135863fc69e4d3f18d33cf44
Kat Juell <kathleenjuell@gmail.com>
2020-02-09 14:41:46 +0000

    Update ssl script and add more to gitignore

--- ssl_renew.sh
+++ ssl_renew.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 COMPOSE="/usr/local/bin/docker-compose --no-ansi"
+DOCKER="/usr/bin/docker"
 
 cd /home/mrcat/art-of-sabrage-site/
-$COMPOSE run certbot renew --dry-run && $COMPOSE kill -s SIGHUP webserver
+$COMPOSE run certbot renew && $COMPOSE kill -s SIGHUP webserver
+$DOCKER system prune -af
